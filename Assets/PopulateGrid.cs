49c9ee957934c63b93ab4d2ca57d123bc00e0414
Ashir Ali <ashirmayankalathil@gmail.com>
2020-01-22 19:12:00 +0530

    Populate Grid using BG Image and Text

--- Assets/PopulateGrid.cs
+++ Assets/PopulateGrid.cs
@@ -1,25 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.UI;
+using UnityEngine.UI;
+using System.IO;
+using UnityEditor;
 public class PopulateGrid : MonoBehaviour
 {
-    public GameObject prefab; // This is our prefab object that will be exposed in the inspector
+    public GameObject prefab; //This is our prefab object that will be exposed in the inspector
 	public int numberToCreate; // number of objects to create. Exposed in inspector
+	Image image; // Create GameObject instance
+	Text text;
+	GameObject imageObject;
 	void Start()
 	{
 		Populate();
 	}
 	void Populate()
 	{
-		//Image newObj; // Create GameObject instance
 		for (int i = 0; i < numberToCreate; i++)
 		{
 			//Create new instances of our prefab until we've created as many as we specified
-			//newObj = (GameObject)Instantiate(prefab, transform);
+			imageObject = (GameObject)Instantiate(prefab, transform);
 			// Randomize the color of our image
-			//newObj.GetComponent<Image>().color = Random.ColorHSV(); 
-		}
+			image = imageObject.GetComponent<Image>();
+			//text = imageObject.
 
+			image.color = Color.black;//Random.ColorHSV();
+			
+		}
 	}
 }
